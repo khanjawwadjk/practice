0fee5328a53a4a4e1f07b80b026b3e2f779caa7d
khanjawwadjk <53745446+khanjawwadjk@users.noreply.github.com>
2021-10-18 23:19:00 +0530

    yet to add functionalities

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Login from './components/Login';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+     <Login/>
+    </div>
+  );
+}
+
+export default App;
